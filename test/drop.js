d83e171661ad76ec79306a454f576e989ce856a2
Kamil <krikus@gmail.com>
2015-08-06 13:07:39 -0700

    Seq.drop tests

---
+++ test/drop.js
@@ -0,0 +1,41 @@
+/* jshint mocha: true, node: true, expr: true */
+"use strict";
+require('harmonize')(['harmony-classes', 'harmony_arrows_functions', 'harmony-proxies']);
+var Seq = require('../src/seq');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Seq drop', function() {
+	it('should work with callback', function() {
+		let seq = new Seq([7,1,2,-1,-5,3,4,6]);
+		let callback = function(x) {
+				return x >= 0;
+		};
+
+		expect(seq.drop(callback).toArray()).to.be.deep.equal([-1,-5,3,4,6]);
+	});
+
+	it('should be able to take numberic parameter', function() {
+		let seq = new Seq(['cat', 'dog', 'mouse']);
+
+		expect(seq.drop(2).toArray()).to.be.deep.equal(['mouse']);
+	});
+
+
+	it('should work with empty Seq', function() {
+		let seq = new Seq([]);
+
+		expect(seq.drop(5).toArray()).to.be.deep.equal([]);
+	});
+
+	it('should be able to take more elements than there are in seq', function() {
+		let array = [7,1,2,-1,-5,3,4,6];
+		let seq = new Seq(array);
+
+		let positive_callback = function(){ return true; };
+		let how_many = 100000;
+
+		expect(seq.drop(how_many).toArray()).to.be.deep.equal([]);
+		expect(seq.drop(positive_callback).toArray()).to.be.deep.equal([]);
+	});
+});
